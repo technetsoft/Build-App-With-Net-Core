18cc68f4ee9f0db454aa12e33ae1854181d05cf1
riyant <riyant.kurniawan@gmail.com>
2016-09-18 15:24:04 +0800

    logging

--- TheWorld/src/TheWorld/Models/WorldRepository.cs
+++ TheWorld/src/TheWorld/Models/WorldRepository.cs
@@ -3,20 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace TheWorld.Models
 {
     public class WorldRepository : IWorldRepository
     {
         private WorldContext _context;
+        private ILogger<WorldRepository> _logger;
 
-        public WorldRepository(WorldContext context)
+        public WorldRepository(WorldContext context, ILogger<WorldRepository> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
         public IEnumerable<Trip> GetAllTrips()
         {
+            _logger.LogInformation("Getting All Trips from the database");
             return _context.Trips.ToList();
         } 
     }
--- TheWorld/src/TheWorld/Startup.cs
+++ TheWorld/src/TheWorld/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using TheWorld.Models;
 using TheWorld.Services;
 
@@ -49,18 +50,27 @@ namespace TheWorld
             services.AddScoped<IWorldRepository, WorldRepository>();
 
             services.AddTransient<WorldContextSeedData>();
+
+            services.AddLogging();
             
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, WorldContextSeedData seeder)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, 
+            WorldContextSeedData seeder,
+            ILoggerFactory factory)
         {
             //app.UseDefaultFiles();
 //#if DEBUG
             if (env.IsEnvironment("Development"))
             {
                 app.UseDeveloperExceptionPage();
+                factory.AddDebug(LogLevel.Information);
+            }
+            else
+            {
+                factory.AddDebug(LogLevel.Error);
             }
 //#endif
             app.UseStaticFiles();
