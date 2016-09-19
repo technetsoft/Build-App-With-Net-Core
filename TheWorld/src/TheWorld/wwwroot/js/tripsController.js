4e1a6247e0988126d8fa8a331ded700929386296
riyant <riyant.kurniawan@gmail.com>
2016-09-19 16:26:54 +0800

    get data using angularjs

--- TheWorld/src/TheWorld/wwwroot/js/tripsController.js
+++ TheWorld/src/TheWorld/wwwroot/js/tripsController.js
@@ -4,10 +4,45 @@
     //Getting Module
     angular.module("app-trips").controller("tripsController", tripsController);
 
-    function tripsController() {
+    function tripsController($http) {
         var viewModel = this;
 
-        viewModel.name = "Riyant";
+        //viewModel.trips = [
+        //{
+        //    name: "US Trip",
+        //    created: new Date()
+        //}, {
+        //    name: "World Trip",
+        //    created: new Date()
+        //}];
+
+        viewModel.trips = [];
+
+        viewModel.errorMessage = "";
+        viewModel.isBusy = true;
+
+        $http.get("/api/trips").then(function(response) {
+            //Success
+            angular.copy(response.data, viewModel.trips);
+        }, function(error) {
+            //Failure
+            viewModel.errorMessage = "Failed to load data: " + error;
+        }).finally(function() {
+            viewModel.isBusy = false;
+        });
+
+        viewModel.newTrip = {};
+
+        viewModel.addTrip = function() {
+            //alert(viewModel.newTrip.name);
+            viewModel.trips.push(
+            {
+                name: viewModel.newTrip.name,
+                created: new Date()
+            });
+
+            viewModel.newTrip = {};
+        };
     }
 
 })();
