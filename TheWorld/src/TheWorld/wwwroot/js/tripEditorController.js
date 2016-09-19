15a5be6841f0a186092f8f63c7e03a268d6d93a3
riyant <riyant.kurniawan@gmail.com>
2016-09-19 19:14:32 +0800

    view editor m

--- TheWorld/src/TheWorld/wwwroot/js/tripEditorController.js
+++ TheWorld/src/TheWorld/wwwroot/js/tripEditorController.js
@@ -14,11 +14,55 @@
         $http.get("/api/trips/"+vm.tripName+"/stops").then(function (response) {
             //Success
             angular.copy(response.data, vm.stops);
+            _showMap(vm.stops);
         }, function (error) {
             //Failure
             vm.errorMessage = "Failed to load stops: " + error;
         }).finally(function () {
             vm.isBusy = false;
         });
+    }
+
+    function _showMap(stops) {
+        if (stops && stops.length > 0) {
+
+            var mapStops = _.map(stops,
+                function(item) {
+                    return {
+                        lat: item.latitude,
+                        long: item.longitude,
+                        info: item.city
+                    };
+                });
+
+            //Show Map
+            travelMap.createMap({
+                stops: mapStops,
+                selector: "#map",
+                currentStop: 1,
+                initialZoom: 3
+            });
+
+            //var map = travelMap.createMap({
+            //    stops: [
+            //        {
+            //            lat: 33.748995,
+            //            long: -84.387982,
+            //            info: "Atlanta, Georgia - Departed Jun 3, 2014"
+            //        },
+            //        {
+            //            lat: 48.856614,
+            //            long: 2.352222,
+            //            info: "Paris, France - Jun 4-24, 2014"
+            //        },
+            //        {
+            //            lat: 50.850000,
+            //            long: 4.350000,
+            //            info: "Brussels, Belgium - Jun 25-27, 2014"
+            //        }
+            //    ],
+            //    selector: "#map"
+            //});
+        }
     }
 })();
