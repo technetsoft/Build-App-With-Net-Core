0f87e04047efb085828403ad588572e8cabcce0e
riyant <riyant.kurniawan@gmail.com>
2016-09-19 18:09:59 +0800

    stops controller

--- TheWorld/src/TheWorld/wwwroot/js/tripEditorController.js
+++ TheWorld/src/TheWorld/wwwroot/js/tripEditorController.js
@@ -3,8 +3,22 @@
 
     angular.module("app-trips").controller("tripEditorController", tripEditorController);
 
-    function tripEditorController() {
+    function tripEditorController($routeParams, $http) {
         var vm = this;
-        vm.name = "Riyant";
+
+        vm.tripName = $routeParams.tripName;
+        vm.stops = [];
+        vm.errorMessage = "";
+        vm.isBusy = true;
+
+        $http.get("/api/trips/"+vm.tripName+"/stops").then(function (response) {
+            //Success
+            angular.copy(response.data, vm.stops);
+        }, function (error) {
+            //Failure
+            vm.errorMessage = "Failed to load stops: " + error;
+        }).finally(function () {
+            vm.isBusy = false;
+        });
     }
 })();
